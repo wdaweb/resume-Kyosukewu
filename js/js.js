da071a21adf797910cd15bc2285201747e2ef07a
Kyosukewu <kyosukewu2020@gmail.com>
2021-01-05 16:12:10 +0800

    js動畫修改

--- js/js.js
+++ js/js.js
@@ -1,24 +1,33 @@
 // $(".mainbg").addClass("wow animate__fadeIn");
+//banner
 $(".bg0").addClass("wow animate__bounceInDown");
 $(".bg1").addClass("wow animate__bounceInDown");
 $(".bg2").addClass("wow animate__backInRight");
 $(".bg3").addClass("wow animate__fadeInLeft");
 $(".banner-txt").addClass("wow animate__fadeIn");
+//about
 $(".tit").addClass("wow animate__slideInLeft");
-$(".tit2").addClass("wow animate__lightSpeedInLeft");
-$(".tit3").addClass("wow animate__slideInLeft");
-$(".tit4").addClass("wow animate__lightSpeedInLeft");
-$(".tit5").addClass("wow animate__slideInLeft");
-$(".tit6").addClass("wow animate__lightSpeedInLeft");
-$(".tit7").addClass("wow animate__slideInLeft");
-$(".tit8").addClass("wow animate__lightSpeedInLeft");
-$(".tit9").addClass("wow animate__slideInLeft");
-$(".tit10").addClass("wow animate__lightSpeedInLeft");
 $(".self").addClass("wow animate__rotateIn");
+//exp
+$(".tit3").addClass("wow animate__slideInLeft");
+$(".li1").addClass("wow animate__rubberBand");
+$(".li2").addClass("wow animate__rubberBand");
+$(".li3").addClass("wow animate__rubberBand");
+$(".li4").addClass("wow animate__rubberBand");
+$(".li5").addClass("wow animate__rubberBand");
+//skill
+$(".tit5").addClass("wow animate__slideInLeft");
 $("#sk1").addClass("wow animate__backInRight");
 $("#sk2").addClass("wow animate__backInRight");
 $("#sk3").addClass("wow animate__backInRight");
 $("#sk4").addClass("wow animate__backInRight");
+//pro
+$(".tit7").addClass("wow animate__slideInLeft");
+$(".proe").addClass("wow animate__bounceIn");
+//cont
+$(".tit9").addClass("wow animate__slideInLeft");
+
+
 new WOW().init();
 
 
